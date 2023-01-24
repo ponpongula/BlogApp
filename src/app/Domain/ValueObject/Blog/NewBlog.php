0c6eba153cbdd9fad3461fa69924ee214095acaa
hayato <rqspss1359@gmail.com>
2023-01-24 14:55:12 +0900

    add:valueObject作成

---
+++ src/app/Domain/ValueObject/Blog/NewBlog.php
@@ -0,0 +1,60 @@
+<?php
+require_once __DIR__ . 'BlogId.php';
+require_once __DIR__ . '/../User/UserId.php';
+require_once __DIR__ . 'Title.php';
+require_once __DIR__ . 'Content.php';
+
+/**
+ * 新規ブログ登録のValueObject
+ */
+final class NewBlog
+{
+    private BlogId $BlogId;
+    private UserId $UserId;
+    private Title $Title;
+    private Content $Content;
+
+    public function __construct(
+        BlogId $BlogId,
+        UserId $UserId,
+        Title $Title,
+        Content $Content
+    ) {
+        $this->BlogId = $BlogId;
+        $this->UserId = $UserId;
+        $this->Title = $Title;
+        $this->Content = $Content;
+    }
+
+    /**
+     * @return BlogId
+     */
+    public function BlogId(): BlogId
+    {
+        return $this->BlogId;
+    }
+
+    /**
+     * @return UserId
+     */
+    public function UserId(): UserId
+    {
+        return $this->UserId;
+    }
+
+    /**
+     * @return Title
+     */
+    public function Title(): Title
+    {
+        return $this->Title;
+    }
+
+    /**
+     * @return Content
+     */
+    public function Content(): Content
+    {
+        return $this->Content;
+    }
+}
