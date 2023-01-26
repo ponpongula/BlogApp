be83f5a0314b6568c753e299d0040c590e0bbd22
hayato <rqspss1359@gmail.com>
2023-01-26 12:51:37 +0900

    add:ReadValueObject作成

---
+++ src/app/Domain/ValueObject/Blog/BlogSearchWord.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * ブログ検索のValueObject
+ */
+final class BlogSerchWord
+{
+    /**
+     * ブログ検索ワードの不正な場合のエラーメッセージ
+     */
+    const INVALID_MESSAGE = '50文字以下でお願いします！';
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * コンストラクタ
+     *
+     * @param string | NULL
+     */
+    public function __construct(?string $value)
+    {
+        if ($this->isInvalid($value)) {
+            throw new Exception(self::INVALID_MESSAGE);
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * @return string | NULL
+     */
+    public function value(): ?string
+    {
+        return $this->value;
+    }
+
+    /**
+     * ブログ検索ワードのバリデーション
+     *
+     * @param string | NULL
+     * @return boolean
+     */
+    private function isInvalid(?string $value): bool
+    {
+        return mb_strlen($value) > 50;
+    }
+}
